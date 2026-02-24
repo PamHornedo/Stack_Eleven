d191d3e56dbf5bd518837ce80ce47c534b35f450
Pamela Hornedo <pamhornedo@gmail.com>
2026-02-23 21:17:53 -0500

    added apollo client to main.jsx

--- Starter/frontend/src/main.jsx
+++ Starter/frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./api.js";
+import App from "./App.jsx";
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
 );
